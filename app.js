36f240dc83681104ba0c2a13b6a6797bf84b2a4b
madenjanna-ui <madenjanna@gmail.com>
2026-07-30 12:21:23 +0400

    Update app.js

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ function renderWeek(){
 
     activateAddButtons();
 
-
+activateTaskSelection();
 
 
 }
@@ -502,83 +502,6 @@ document
 };
 
 
-// =====================================
-// Выбор задачи
-// =====================================
-
-function activateTaskSelection(){
-
-    document
-    .querySelectorAll(".task")
-    .forEach(task=>{
-
-
-        task.onclick=function(e){
-
-
-            if(
-                e.target.tagName==="INPUT"
-            ){
-
-                return;
-
-            }
-
-
-
-            document
-            .querySelectorAll(".task")
-            .forEach(t=>{
-
-                t.classList.remove(
-                    "selected"
-                );
-
-            });
-
-
-
-            task.classList.add(
-                "selected"
-            );
-
-
-
-            let day =
-            task.closest(".day");
-
-
-
-            selectedTask={
-
-                date:
-                day.dataset.date,
-
-
-                index:
-                Array
-                .from(
-                    day.querySelectorAll(".task")
-                )
-                .indexOf(task)
-
-            };
-
-
-            showTaskMenu(
-                task,
-                tasks[selectedTask.date][selectedTask.index],
-                selectedTask.date,
-                selectedTask.index
-            );
-
-
-        };
-
-
-    });
-
-}
 
 
 
